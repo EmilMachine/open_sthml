e6f34cdebba9f3c431d44dbede9521ee809b8392
EmilMachine <emil.lime@gmail.com>
2016-09-06 23:11:58 +0200

    added converted to pandas obj

--- emil/load_data.py
+++ emil/load_data.py
@@ -1,5 +1,6 @@
 import urllib2
 from bs4 import BeautifulSoup
+import pandas as pd
 
 ### GET RAW XML
 data_path = 'http://data.stockholm.se/set/Befolkning/Flyttningar/?apikey=S8O2CD27J2K4EE55CED0L0J5H81A1902'
@@ -8,18 +9,30 @@ page = urllib2.urlopen(data_path).read()
 ### PARSE XML
 soup = BeautifulSoup(page,'lxml')
 
-### xml specific / todo generalize
-def soup2list(soup,key):
-	tmp = [elem.string for elem in soup.findAll(key)]
+### Extract list (usefull if assume table structure)
+def soup2list(soup,key,method=str):
+	tmp = [method(elem.string) for elem in soup.findAll(key)]
 	return tmp
 
-area_code = soup2list(soup,'AREA_CODE')
-year = soup2list(soup,'year')
-moved_code = soup2list(soup, 'FLYTT3K_CODE')
-moved = soup2list(soup, 'FLYTT6_ANTAL')
+### xml specific / todo generalize?
+# BYHAND
+area_code = soup2list(soup,'area_code')
+year = soup2list(soup,'year',int)
+gender_code = soup2list(soup,'konk_code')
+moved_code = soup2list(soup, 'flytt3k_code',int)
+moved = soup2list(soup, 'flytt6_antal',int)
+### TODO: FIX UNICODE ERROR
+#area_text = soup2list(soup,'area_text')
+#moved_text = soup2list(soup, 'flytt3k_text')
+#gender = soup2list(soup,'konk_text')
 
-# TODO add type casting (to int etc)
-# TODO convert to pandas obj
+### convert to pandas obj
+# BYHAND
+df = pd.DataFrame.from_records(
+	zip(area_code,year,gender_code,moved_code,moved)
+	, columns = ['area_code','year','gender_code','moved_code','moved']
+	)
+
 # TODO simple plot 
 
 

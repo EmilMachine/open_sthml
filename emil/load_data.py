f1425524b398b8f3b0063cfcb4af2150de359ef0
EmilMachine <emil.lime@gmail.com>
2016-09-07 08:47:16 +0200

    added simple groupby and plotting to example

--- emil/load_data.py
+++ emil/load_data.py
@@ -1,6 +1,7 @@
 import urllib2
 from bs4 import BeautifulSoup
 import pandas as pd
+import matplotlib.pyplot as plt
 
 ### GET RAW XML
 data_path = 'http://data.stockholm.se/set/Befolkning/Flyttningar/?apikey=S8O2CD27J2K4EE55CED0L0J5H81A1902'
@@ -33,8 +34,20 @@ df = pd.DataFrame.from_records(
 	, columns = ['area_code','year','gender_code','moved_code','moved']
 	)
 
-# TODO simple plot 
 
+### simple plot 
+# 1. aggregate
+df2 = df[['year','gender_code','moved']].groupby(['year','gender_code']).sum()
+
+# 2. plot
+# See http://stackoverflow.com/questions/28293028/plotting-grouped-data-in-same-plot-using-pandas
+fig, ax = plt.subplots(figsize=(8,6))
+
+# See http://stackoverflow.com/questions/25386870/pandas-plotting-with-multi-index
+df2.unstack(level=0).plot(kind='bar',ax=ax)
+plt.ylabel('people moved')
+
+plt.show()
 
 
 # Find fields in tag (http://stackoverflow.com/questions/10947968/xml-to-pandas-dataframe)

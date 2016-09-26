59a1e2a740dbce637e4ca3f2a4e1e6bc9ed050af
filipclaesson <filipclaesson@hotmail.com>
2016-09-27 00:27:36 +0200

    handle data

--- filip/api_caller.js
+++ filip/api_caller.js
@@ -1,5 +1,4 @@
 //http://stackoverflow.com/questions/14018269/how-to-post-xml-data-in-node-js-http-request
-
 var http = require('http');
 var request = require('request')
 //var body = '<REQUEST><LOGIN authenticationkey="5b1a60214fdd45b5bfc5166d2c95d30d" /><QUERY objecttype="Situation" limit="5"><FILTER><WITHIN name="Deviation.Geometry.SWEREF99TM" shape="center" value="674585 6580201" radius="10000" /></FILTER><INCLUDE>Deviation.Header</INCLUDE><INCLUDE>Deviation.IconId</INCLUDE><INCLUDE>Deviation.Message</INCLUDE><INCLUDE>Deviation.MessageCode</INCLUDE><INCLUDE>Deviation.MessageType</INCLUDE></QUERY></REQUEST>';
